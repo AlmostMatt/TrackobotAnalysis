ee4b0e58401b1aa4ba7a9afaff5680e45d7953d0
Matthew Hyndman <myndman@gmail.com>
2016-05-02 22:07:24 -0700

    updated config

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import pytz
 
-USERNAME="cold-gruul-1615"
-PASSWORD="abda5488b1"
+USERNAME="word-word-1234"
+PASSWORD="aaaa1111a1"
 LOCAL_TZ = pytz.timezone('US/Pacific') # pytz timezone name
-API_KEY="8kxEaFy6PuWpy4EX4yQf"
-
+API_KEY="1aa1aAAa1a1A1a1a1"
